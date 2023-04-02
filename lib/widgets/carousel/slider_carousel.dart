802c6a4bc7fab60886db192dca787d0f3db10fbd
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2023-04-02 14:16:47 -0300

    Estilizando Carousel de personagens

--- lib/widgets/carousel/slider_carousel.dart
+++ lib/widgets/carousel/slider_carousel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../configurations/colors.dart';
 import '../../models/results.dart';
 
 class SliderCarousel extends StatelessWidget {
@@ -22,11 +23,44 @@ class SliderCarousel extends StatelessWidget {
       duration: const Duration(milliseconds: 500),
       curve: Curves.easeInOutCubic,
       margin: EdgeInsets.all(margin),
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              image: NetworkImage(
-                  "${charactersList[index].thumbnail.path}.${charactersList[index].thumbnail.extension}"),
-              fit: BoxFit.fill)),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.network(
+            "${charactersList[index].thumbnail.path}.${charactersList[index].thumbnail.extension}",
+            fit: BoxFit.fill,
+          ),
+          Positioned(
+            height: MediaQuery.of(context).size.height / 10,
+            right: 0,
+            bottom: 0,
+            child: Card(
+              color: primaryBlack,
+              child: Column(
+                children: [
+                  Flexible(
+                    child: Text(
+                      charactersList[index].name,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.redAccent),
+                    ),
+                  ),
+                  Text(
+                    "Quadrinhos: ${charactersList[index].comics.available.toString()} - Séries: ${charactersList[index].series.available.toString()}",
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                  Text(
+                    "Histórias: ${charactersList[index].stories.available.toString()} - Eventos: ${charactersList[index].events.available.toString()}",
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
